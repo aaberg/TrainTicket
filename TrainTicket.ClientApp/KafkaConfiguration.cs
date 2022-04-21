c6c12201b4759e51aad25d8b809dc3a88522ad06
Lars Aaberg <lars@aaberg.cc>
2022-04-21 21:11:18 +0200

    Beginning on client

---
+++ TrainTicket.ClientApp/KafkaConfiguration.cs
@@ -0,0 +1,9 @@
+﻿namespace TrainTicket.ClientApp;
+
+public class KafkaConfiguration
+{
+    public string BootstrapServers { get; set; }
+    public string GroupId { get; set; }
+    
+    public string StationTopic = "station-events";
+}
--- TrainTicket.ClientApp/Program.cs
+++ TrainTicket.ClientApp/Program.cs
@@ -1,7 +1,21 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using Confluent.Kafka;
 using Microsoft.Extensions.Configuration;
+using TrainTicket.ClientApp;
 
 Console.WriteLine("Starting application...");
 
 IConfiguration config = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build();
+var kafkaConfig = new KafkaConfiguration();
+config.Bind("kafka", kafkaConfig);
+
+
+var kafkaConsumerConfig = new ConsumerConfig
+{
+    BootstrapServers = kafkaConfig.BootstrapServers,
+    GroupId = kafkaConfig.GroupId,
+    AutoOffsetReset = AutoOffsetReset.Earliest
+};
+
+
